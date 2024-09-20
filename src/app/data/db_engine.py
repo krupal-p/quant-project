ab759c02512dce125c93cdadb517597b165b172b
Krupal <krupal.patel.23@gmail.com>
2024-09-20 14:02:19 -0400

    Refactor DBEngine to use get_config and get_logger functions

--- src/app/data/db_engine.py
+++ src/app/data/db_engine.py
@@ -1,10 +1,33 @@
+import threading
+
+import pandas as pd
+import polars as pl
 from sqlalchemy import Engine, create_engine
 
-from app import config
+from app import get_config, get_logger
 
 
-def get_db_conn(url: str) -> Engine:
-    return create_engine(url)
+class DBEngine:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                cls._instance = super().__new__(cls)
+                cls.log = get_logger()
+                cls.log.info("Creating DBEngine instance")
+                cls.conn: Engine = create_engine(get_config().SQLSERVER_URL)
+        return cls._instance
 
 
-sqlserver_conn: Engine = get_db_conn(config.SQLSERVER_URL)
+def get_db_conn() -> Engine:
+    return DBEngine().conn
+
+
+def read_sql(query: str) -> pd.DataFrame:
+    return pd.read_sql(query, get_db_conn(), dtype_backend="pyarrow")
+
+
+def read_sql_polar(query: str) -> pl.DataFrame:
+    return pl.read_database(query, get_db_conn())
