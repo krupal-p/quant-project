a4cb40ea761fc2cd0ac3eb978673f643d07cd124
Krupal <krupal.patel.23@gmail.com>
2024-09-20 14:03:48 -0400

    Refactor DB connection tests and add new SQL read tests

--- tests/integration/test_db_conn.py
+++ tests/integration/test_db_conn.py
@@ -1,8 +1,19 @@
-from app.data import db_engine
+from app.data.db_engine import get_db_conn, read_sql, read_sql_polar
 from sqlalchemy import text
 
 
 def test_sql_server_conn():
-    with db_engine.sqlserver_conn.connect() as conn:
+    with get_db_conn().connect() as conn:
         result = conn.execute(text("SELECT 1"))
         assert result.fetchone()[0] == 1
+
+
+def test_read_sql():
+    df = read_sql("SELECT 1")
+    assert df.shape == (1, 1)
+    assert df.iloc[0, 0] == 1
+
+
+def test_read_sql_polar():
+    df = read_sql_polar("SELECT 1")
+    assert df.shape == (1, 1)
