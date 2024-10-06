4e7984064042d7487a1a585e081dd2a23b32854e
Krupal <krupal.patel.23@gmail.com>
2024-10-06 16:00:37 -0400

    Refactor database related functions and imports
    - Updated the `insert_into_table` function in `db.py` to accept a list of dictionaries for data insertion.
    - Added a new function `execute_sql_statement_from_file` in `db.py` to execute SQL statements from files.
    - Removed unused imports in `utils.py`.

--- src/app/common/db.py
+++ src/app/common/db.py
@@ -1,9 +1,13 @@
 import threading
+from pathlib import Path
+from typing import Any
 
 import pandas as pd
 import polars as pl
 from app import config, log
-from sqlalchemy import Engine, create_engine, text
+from sqlalchemy import Engine, MetaData, Table, create_engine, text
+
+metadata_obj = MetaData()
 
 
 class DBEngine:
@@ -32,13 +36,31 @@ def read_sql_polar(query: str) -> pl.DataFrame:
 
 
 def insert_into_table(
-    insert_statement: str,
-    data,
-    table_name_with_schema: str,
+    data: list[dict[str, Any]],
+    table_name: str,
+    schema: str,
     *,
     truncate: bool,
-) -> None:
+):
+    table = Table(
+        table_name,
+        metadata_obj,
+        schema=schema,
+        autoload_with=get_db_conn(),
+    )
     with get_db_conn().begin() as conn:
         if truncate:
-            conn.execute(text(f"TRUNCATE TABLE {table_name_with_schema}"))
-        conn.execute(text(insert_statement), data)
+            conn.execute(text(f"TRUNCATE TABLE {schema}.{table_name}"))
+        conn.execute(table.insert(), data)
+
+
+def execute_sql_statement_from_file(file_name: str) -> None:
+    """
+    Execute a SQL statement from a file in the sql directory without the sql extension.
+    """
+    file_path = Path(__file__).parent.parent / "sql" / (file_name + ".sql")
+
+    with Path(file_path).open() as file:
+        sql_statement = file.read()
+        with get_db_conn().begin() as conn:
+            conn.execute(text(sql_statement))
--- src/app/common/utils.py
+++ src/app/common/utils.py
@@ -1,19 +1,4 @@
 import re
-from pathlib import Path
-
-from sqlalchemy import Engine, text
-
-
-def execute_sql_statement_from_file(file_name: str, engine: Engine) -> None:
-    """
-    Execute a SQL statement from a file in the sql directory without the sql extension.
-    """
-    file_path = Path(__file__).parent.parent / "sql" / (file_name + ".sql")
-
-    with Path(file_path).open() as file:
-        sql_statement = file.read()
-        with engine.begin() as conn:
-            conn.execute(text(sql_statement))
 
 
 def to_snake_case(s):
