a5f0d2befd90a31a443d790437e26bf4562a4157
Krupal <krupal.patel.23@gmail.com>
2024-10-06 16:01:36 -0400

    Refactor SP500IndexConstituentsUpdater class to save constituents and update dim_index_tracker table

--- src/app/mkt_data/update_index_constituents.py
+++ src/app/mkt_data/update_index_constituents.py
@@ -1,23 +1,25 @@
-from app.common.db import get_db_conn, insert_into_table
-from app.common.utils import execute_sql_statement_from_file
+from typing import TYPE_CHECKING
+
+from app.common.db import (
+    execute_sql_statement_from_file,
+    get_db_conn,
+    insert_into_table,
+)
 from app.mkt_data.market_data import get_sp500_constituents
+
+if TYPE_CHECKING:
+    from app.mkt_data.models import SP500Constituent
 
 
 class SP500IndexConstituentsUpdater:
     def __init__(self):
-        self.constituents = get_sp500_constituents()
+        self.constituents: list[SP500Constituent] = get_sp500_constituents()
 
     def save_constituents(self, constituents):
         # Update the dim_index_tracker table with any new S&P 500 constituents
+        insert_into_table(constituents, "dim_index_tracker", "stg", truncate=True)
 
-        insert_into_table(
-            "INSERT INTO stg.dim_index_tracker (index_name, symbol, security_name, gics_sector, gics_sub_industry, date_added) VALUES (:index_name, :symbol, :security_name, :gics_sector, :gics_sub_industry, :date_added)",
-            constituents,
-            "stg.dim_index_tracker",
-            truncate=True,
-        )
-
-        execute_sql_statement_from_file("merge_sp500_constituents", get_db_conn())
+        execute_sql_statement_from_file("merge_sp500_constituents")
 
     def main(self):
         self.save_constituents(
