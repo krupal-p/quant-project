5b76d9330b4d736d2f171b42d8b457daa388d410
Krupal <krupal.patel.23@gmail.com>
2025-01-18 17:07:38 -0500

    Refactor S&P 500 index constituents update process to use DLT pipeline and remove obsolete updater class

--- src/app/ui/jobs/update_sp500_index_constituents.py
+++ src/app/ui/jobs/update_sp500_index_constituents.py
@@ -1,11 +1,36 @@
+import dlt
 from app import log
-from app.mkt_data.update_index_constituents import SP500IndexConstituentsUpdater
+from app.common.db import execute_sql_statement, generate_merge_statement
+from app.mkt_data.market_data import get_sp500_constituents
 
 
 def main():
-    sp500_index_constituents_updater = SP500IndexConstituentsUpdater()
-    sp500_index_constituents_updater.main()
-    log.info("Finished updating S&P 500 index constituents")
+    # get the S&P 500 index constituents
+    sp500_constituents = get_sp500_constituents()
+
+    # save the S&P 500 index constituents to the database
+    pipeline = dlt.pipeline(
+        pipeline_name="sp500_index_constituents",
+        destination="mssql",
+        dataset_name="stg",
+    )
+
+    pipeline.run(
+        sp500_constituents,
+        table_name="sp500_index_constituents",
+        dataset_name="stg",
+        write_disposition="replace",
+    )
+
+    execute_sql_statement(
+        generate_merge_statement(
+            "sp500_index_constituents",
+            "stg",
+            "dim_index_tracker",
+            "dbo",
+        ),
+    )
+    log.info("S&P 500 index constituents have been updated.")
 
 
 if __name__ == "__main__":
