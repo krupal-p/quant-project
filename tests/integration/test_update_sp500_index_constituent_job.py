6742c71f68d5d4aefa0c648c3991eed6dd83407e
Krupal <krupal.patel.23@gmail.com>
2025-01-18 17:26:10 -0500

    Remove obsolete integration test for SP500IndexConstituentsUpdater

--- tests/integration/test_update_sp500_index_constituent_job.py
+++
@@ -1,6 +0,0 @@
-from app.mkt_data.update_index_constituents import SP500IndexConstituentsUpdater
-
-
-def test_save_constituents():
-    sp500_index_constituents_updater = SP500IndexConstituentsUpdater()
-    sp500_index_constituents_updater.main()
