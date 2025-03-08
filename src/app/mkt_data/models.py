8fffe5136d08222d41177814409b99a422d7cd5c
Krupal <krupal.patel.23@gmail.com>
2025-03-08 11:52:24 -0500

    Refactor market data models by removing unused classes and imports

--- src/app/mkt_data/models.py
+++ src/app/mkt_data/models.py
@@ -1,8 +1,6 @@
 from datetime import date
-from decimal import Decimal
-from uuid import UUID
 
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field
 
 
 class SP500Constituent(BaseModel):
@@ -25,41 +23,3 @@ class Security(BaseModel):
     industry_key: str = Field(alias="industryKey")
     sector_key: str = Field(alias="sectorKey")
     long_business_summary: str = Field(alias="longBusinessSummary")
-
-
-class MarketData(BaseModel):
-    symbol: str = Field(..., max_length=14)
-    item_id: int = Field(...)
-    value_date: date = Field(...)
-    value: Decimal = Field(
-        ...,
-        max_digits=30,
-    )
-
-    @field_validator("value", mode="before")
-    @classmethod
-    def validate_value(cls, value: float | int) -> Decimal | int:
-        if isinstance(value, int):
-            return value
-        return Decimal(value).quantize(Decimal("0.0001"), rounding="ROUND_HALF_UP")
-
-
-class SecMaster(BaseModel):
-    symbol: str
-    underlying_symbol: str
-    isin: str
-    short_name: str
-    long_name: str
-    quote_type: str
-    currency: str
-    exchange: str
-    industry_key: str
-    sector_key: str
-    uuid: UUID
-    long_business_summary: str
-
-
-class ItemMaster(BaseModel):
-    item_id: int
-    item_name: str
-    item_desc: str
