a66172c0f9672fe69d008045210769caeb45944b
Krupal <krupal.patel.23@gmail.com>
2025-03-08 11:52:18 -0500

    Add resource caching for ProcessPoolExecutor in Streamlit app

---
+++ src/app/ui/resources.py
@@ -0,0 +1,11 @@
+from concurrent.futures import ProcessPoolExecutor
+
+import streamlit as st
+
+from app import log
+
+
+@st.cache_resource
+def get_executor() -> ProcessPoolExecutor:
+    log.info("Initializing executor")
+    return ProcessPoolExecutor(max_workers=2)
