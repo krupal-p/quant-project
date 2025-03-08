e5f1e405fcdb554afa8640ba5927cf327e792f25
Krupal <krupal.patel.23@gmail.com>
2025-03-08 11:35:54 -0500

    Remove obsolete SQL initialization script and integration test for market data

--- tests/integration/test_quant_db.py
+++
@@ -1,8 +0,0 @@
-from app.mkt_data.models import SecMaster
-from app.mkt_data.quant_db import get_all_securities
-
-
-def test_get_all_securities():
-    securties: list[SecMaster] = get_all_securities()
-
-    assert securties is not None
