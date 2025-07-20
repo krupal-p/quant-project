9542b220970c0e22435169c5bbe182d857036fa5
Krupal <krupal.patel.23@gmail.com>
2025-07-20 17:35:15 -0400

    Add task and flow state hooks to send email notifications on completion

---
+++ src/app/flows/flow_utils.py
@@ -0,0 +1,21 @@
+from app.common.email_sender import email_send_message
+from prefect import Flow, Task
+from prefect.client.schemas.objects import FlowRun, State, TaskRun
+
+
+def task_state_hook(task: Task, task_run: TaskRun, state: State) -> None:
+    email_send_message(
+        subject=f"Task {task.name} run {task_run.id} completed",
+        msg=f"Task {task.name} run {task_run.id} has completed with state: {state}",
+        email_from="test",
+        email_to="recipient@example.com",
+    )
+
+
+def flow_state_hook(flow: Flow, flow_run: FlowRun, state: State) -> None:
+    email_send_message(
+        subject=f"Flow {flow.name} run {flow_run.id} completed",
+        msg=f"Flow {flow.name} run {flow_run.id} has completed with state: {state.data}",
+        email_from="test",
+        email_to="recipient@example.com",
+    )
